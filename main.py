99ae3b605f97d65c17b2dfe1dec4edd86751b8da
Bruno-MdeMaria <106575354+Bruno-MdeMaria@users.noreply.github.com>
2022-07-16 01:16:39 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import random
+word_list = ["aardvark", "baboon", "camel"]
+#DO-1 - Escolha aleatoriamente uma palavra da lista_palavras e atribua-a a uma variável chamada palavra_escolhida.
+
+#TO-2 - Peça ao usuário que adivinhe uma letra e atribua sua resposta a uma variável chamada guess. Faça adivinhar minúsculas.
+
+#T-3 - Verifique se a letra que o usuário adivinhou é uma das letras da palavra escolhida.
+palavra_escolhida = random.choice(word_list)
+print(palavra_escolhida)
+guess = input("Diga uma letra que tenha na palvra? = ").lower()
+print(guess)
