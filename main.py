6874e8ebcb33a8daa3a54ac7077d324892888d22
Bruno-MdeMaria <106575354+Bruno-MdeMaria@users.noreply.github.com>
2022-07-17 09:18:04 -0300

    passo 2
    acrscentao display

--- main.py
+++ main.py
@@ -5,11 +5,18 @@ word_list = ["aardvark", "baboon", "camel"]
 #TO-2 - Peça ao usuário que adivinhe uma letra e atribua sua resposta a uma variável chamada guess. Faça adivinhar minúsculas.
 
 #T-3 - Verifique se a letra que o usuário adivinhou é uma das letras da palavra escolhida.
-palavra_escolhida = list(random.choice(word_list))
-print(palavra_escolhida)
-guess = input("Diga uma letra que tenha na palvra? = ").lower()
+palavra_escolhida = random.choice(word_list)
+print(f'Pssst, the solution is {palavra_escolhida}.')
+guess = input("Diga uma letra que tenha na palavra? = ").lower()
 print(guess)
+
+display = []
 for letra in palavra_escolhida:
+    display += "_"
+print(display)
+
+for posicao in range(len(palavra_escolhida)):
+    letra = palavra_escolhida[posicao]
     if letra == guess:
-        print(guess)
-    else: print("foi mal")
+        display[posicao] = letra
+print(display)
