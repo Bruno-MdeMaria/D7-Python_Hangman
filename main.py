cf329dbb394fcfcf6b6821073df20d0df448a88c
Bruno-MdeMaria <106575354+Bruno-MdeMaria@users.noreply.github.com>
2022-07-17 17:55:34 -0300

    ordem para loop de nova tentativa

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ word_list = ["camelo", "elefante", "gato"]
 palavra = random.choice(word_list)
 print(f'Pssst, the solution is {palavra}.')
 
-verificar = False
-while verificar == False:
+display = []
+for letra in palavra:
+    display += "_"
+print(display)
+
+fim_game = False
+while fim_game == False:
     escolha = input("Diga uma letra que tenha na palavra? = ").lower()
     print(escolha)
-
-    display = []
-    for letra in palavra:
-        display += "_"
-    print(display)
 
     for posicao in range(len(palavra)):
         letra = palavra[posicao]
@@ -20,5 +20,5 @@ while verificar == False:
             display[posicao] = letra
     print(display)
     if "_" not in display:
-        verificar = True
+        fim_game = True
         print("Você venceu!")
