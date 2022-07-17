45ae77c63edf6d5d02fc7c5f21137c5282ac83b0
Bruno-MdeMaria <106575354+Bruno-MdeMaria@users.noreply.github.com>
2022-07-17 17:27:48 -0300

    passo 3

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 import random
-word_list = ["aardvark", "baboon", "camel"]
-#DO-1 - Escolha aleatoriamente uma palavra da lista_palavras e atribua-a a uma variável chamada palavra_escolhida.
+word_list = ["camelo", "elefante", "gato"]
 
-#TO-2 - Peça ao usuário que adivinhe uma letra e atribua sua resposta a uma variável chamada guess. Faça adivinhar minúsculas.
+palavra = random.choice(word_list)
+print(f'Pssst, the solution is {palavra}.')
 
-#T-3 - Verifique se a letra que o usuário adivinhou é uma das letras da palavra escolhida.
-palavra_escolhida = random.choice(word_list)
-print(f'Pssst, the solution is {palavra_escolhida}.')
-guess = input("Diga uma letra que tenha na palavra? = ").lower()
-print(guess)
+verificar = False
+while verificar == False:
+    escolha = input("Diga uma letra que tenha na palavra? = ").lower()
+    print(escolha)
 
-display = []
-for letra in palavra_escolhida:
-    display += "_"
-print(display)
+    display = []
+    for letra in palavra:
+        display += "_"
+    print(display)
 
-for posicao in range(len(palavra_escolhida)):
-    letra = palavra_escolhida[posicao]
-    if letra == guess:
-        display[posicao] = letra
-print(display)
+    for posicao in range(len(palavra)):
+        letra = palavra[posicao]
+        if letra == escolha:
+            display[posicao] = letra
+    print(display)
+    if "_" not in display:
+        verificar = True
+        print("Você venceu!")
