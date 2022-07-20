43e9f1223f34ad214b4f8917637333749630b6e6
Bruno-MdeMaria <106575354+Bruno-MdeMaria@users.noreply.github.com>
2022-07-20 02:01:44 -0300

    diversos

--- main.py
+++ main.py
@@ -1,24 +1,49 @@
 import random
-word_list = ["camelo", "elefante", "gato"]
+from imagens import vidas
+from imagens import logo
+from lista_nomes import word_list
 
+
+fim_game = False
+vida = 6
 palavra = random.choice(word_list)
-print(f'Pssst, the solution is {palavra}.')
+#print(f'Pssst, the solution is {palavra}.')
+
+print(logo)
 
 display = []
 for letra in palavra:
     display += "_"
-print(display)
+qtde_letras = len(display)
 
-fim_game = False
+print("BEM VINDO AO HANGMAN - JOGO DA FORCA!")
+print(vidas[vida])
+print(f"É um animal com {qtde_letras} letras: ")
+print(f"{' '.join(display)}\n")
+
+
+
 while fim_game == False:
-    escolha = input("Diga uma letra que tenha na palavra? = ").lower()
+    escolha = input("Diga uma letra que tenha no nome desse animal? = ").lower()
     print(escolha)
 
     for posicao in range(len(palavra)):
         letra = palavra[posicao]
         if letra == escolha:
             display[posicao] = letra
-    print(display)
+    
+    
+    if escolha not in letra:
+        print("Esta palavra não tem esta letra. Você perdeu uma vida!")
+        vida -= 1
+        if vida == 0:
+          fim_game = True
+          print("Você perdeu a sua última chance: \nGame Over!") 
+
+    print(vidas[vida])
+    
+    print(f"{' '.join(display)}")
+
     if "_" not in display:
         fim_game = True
         print("Você venceu!")
